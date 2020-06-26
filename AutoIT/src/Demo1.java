bf3f6ad53fc0219e14f6d5b6ae5acd04ff269b20
Hp <Hp@LAPTOP-AD0BKUHC>
2020-06-26 17:33:56 +0530

    Anurag first Project

---
+++ AutoIT/src/Demo1.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("https://www.sodapdf.com/word-to-pdf/");
+		 
+		 driver.manage().window().maximize();
+		 
+		 driver.findElement(By.xpath("//label[@class='choose-label']")).click();
+		 
+		 Runtime.getRuntime().exec("C:\\Users\\Hp\\Desktop\\Autoit\\document.exe");
+
+	}
+
+}
